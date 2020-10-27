30ae7ff4ef2aabede42787b458c89e6e680cddd5
Lindany <mabasolindaniricardo@gmail.com>
2020-10-27 20:48:15 +0200

    adding the website

---
+++ dripclean/walkfresh.co.za/xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://walkfresh.co.za</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://walkfresh.co.za/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://walkfresh.co.za/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://walkfresh.co.za/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://walkfresh.co.za/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://walkfresh.co.za/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
